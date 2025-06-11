6533b50c2ddd761fd5231a9ffab9e21970b6f7bf
Ken <kenetdeabreu24@gmail.com>
2025-06-11 06:00:54 +0200

    Update sma_cross.py

--- strategy/sma_cross.py
+++ strategy/sma_cross.py
@@ -1,1 +1,15 @@
+class SMACrossStrategy:
+    def generate_signal(self, data):
+        candles = data["history"]
+        closes = [c["close"] for c in candles]
+        if len(closes) < 20:
+            return None
 
+        sma5 = sum(closes[-5:]) / 5
+        sma10 = sum(closes[-10:]) / 10
+
+        if sma5 > sma10:
+            return {"signal": "up"}
+        elif sma5 < sma10:
+            return {"signal": "down"}
+        return None
