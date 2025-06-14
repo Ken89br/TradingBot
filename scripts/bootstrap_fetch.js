2b3769b3c19c510d3bf74d0c62cbb3a8589d78a2
Ken <kenetdeabreu24@gmail.com>
2025-06-14 19:34:44 +0200

    Update bootstrap_fetch.js

--- scripts/bootstrap_fetch.js
+++ scripts/bootstrap_fetch.js
@@ -1,7 +1,7 @@
 // strategy/bootstrap_fetch.js
 const fs = require("fs");
 const path = require("path");
-const dukas = require("dukascopy-node");
+const dukas = require("dukascopy");
 const dayjs = require("dayjs");
 
 const symbols = [
