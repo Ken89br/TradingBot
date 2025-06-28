bacc3a7149a104cd58fe81a73411a68b15a4ed20
Ken <kenetdeabreu24@gmail.com>
2025-06-28 07:59:52 +0200

    Update ensemble_strategy.py

--- strategy/ensemble_strategy.py
+++ strategy/ensemble_strategy.py
@@ -44,7 +44,7 @@ class EnsembleStrategy:
             RSIStrategy(),
             SMACrossStrategy(),
             BollingerStrategy(),
-            PriceActionStrategy(),
+            PriceActionStrategy(CONFIG["price_action"]),
             EMAStrategy(),
             ATRStrategy(),
             ADXStrategy(),
