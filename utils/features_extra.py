e5736f90196a6ab00b88755133bb019fe674af0d
Ken89F <kendeabreu24@gmail.com>
2025-07-09 11:02:14 +0200

    Add files via upload

---
+++ utils/features_extra.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def calc_obv(df):
+    obv = [0]
+    for i in range(1, len(df)):
+        if df['close'].iloc[i] > df['close'].iloc[i-1]:
+            obv.append(obv[-1] + df['volume'].iloc[i])
+        elif df['close'].iloc[i] < df['close'].iloc[i-1]:
+            obv.append(obv[-1] - df['volume'].iloc[i])
+        else:
+            obv.append(obv[-1])
+    return pd.Series(obv, index=df.index)
